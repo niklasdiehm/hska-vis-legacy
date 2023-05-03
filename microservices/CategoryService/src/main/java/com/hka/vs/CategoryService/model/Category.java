d5fc849b9c98e64b1e9a7893595185e41c78624f
niklasdiehm <niklas.diehm@web.de>
2023-05-03 10:42:55 +0200

    use category, create new sql server

---
+++ microservices/CategoryService/src/main/java/com/hka/vs/CategoryService/model/Category.java
@@ -0,0 +1,46 @@
+package com.hka.vs.CategoryService.model;
+
+import jakarta.persistence.*;
+
+/**
+ * This class contains details about categories.
+ */
+@Entity
+@Table(name = "category")
+public class Category implements java.io.Serializable {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private int id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "name", nullable = false)
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ microservices/CategoryService/src/main/java/com/hka/vs/CategoryService/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.hka.vs.CategoryService.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hka.vs.CategoryService.model.Category;
+
+@Repository
+public interface CategoryRepository<T, ID> extends CrudRepository<Category, Integer> {
+    Category findByName(String name);
+}
